96d199db831d7a66cf8a545ba78db0ef7c007cff
Sporty9Coder <126346243+Sporty9Coder@users.noreply.github.com>
2024-03-25 00:01:43 +0530

    removed category from profile

--- src/Project/GrowerProfile.jsx
+++ src/Project/GrowerProfile.jsx
@@ -159,25 +159,6 @@ async function doUpdateWithAxios()
               </div>
             </div>
 
-            <div className="sm:col-span-3">
-              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
-                Categories
-              </label>
-              <div className="mt-2">
-                <select
-                  id="category"
-                  name="category"
-                  autoComplete="category-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6" onChange={doUpdateGrowerObj} value={growerobj.category}
-                >
-                  <option>Select category</option>
-                  <option>Milk Products</option>
-                  <option>Vegetables</option>
-                  <option>Fruits</option>
-                </select>
-              </div>
-            </div>
-
             <div className="col-span-full">
               <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
                 Address
