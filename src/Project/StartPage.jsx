8b2f32dd51d53926f43b73369d6a70aef0b9f372
Sporty9Coder <126346243+Sporty9Coder@users.noreply.github.com>
2024-03-27 23:10:19 +0530

    css change

--- src/Project/StartPage.jsx
+++ src/Project/StartPage.jsx
@@ -59,17 +59,17 @@ export default function StartPage() {
                       <Menu.Item>
                         {({ active }) => (
                           <button
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 text-center')}
                           >
                             Account Settings
                           </button>
                         )}
                       </Menu.Item>
-                      <Menu.Item className='text-center'>
+                      <Menu.Item>
                         {({ active }) => (
                           <button
                             onClick={logout}
-                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700 text-center')}
                           >
                             Sign out
                           </button>
