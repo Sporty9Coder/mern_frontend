d1d21b795a6375e0e97ca23b3ada1a9cb6b0f60e
Sporty9Coder <126346243+Sporty9Coder@users.noreply.github.com>
2024-04-16 17:26:14 +0530

    some change

--- src/Project/Components/Others/SignUp.jsx
+++ src/Project/Components/Others/SignUp.jsx
@@ -57,7 +57,7 @@ export default function SignUp() {
                   name="emailid"
                   type="email"
                   autoComplete="email"
-                  required disabled={!isValid}
+                  required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={doUpdate} onBlur={checkValidEmail}
                 />
               </div>
