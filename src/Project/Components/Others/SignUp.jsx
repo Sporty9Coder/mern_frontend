59bcfbe747ad52654e5e8e659f970a80fdc1479b
Sporty9Coder <126346243+Sporty9Coder@users.noreply.github.com>
2024-04-16 17:24:20 +0530

    handled logical error in validation

--- src/Project/Components/Others/SignUp.jsx
+++ src/Project/Components/Others/SignUp.jsx
@@ -14,11 +14,13 @@ export default function SignUp() {
 
   function doUpdate(event) {
     const { name, value } = event.target;
-    if (name === "emailid") {
-      setIsValid(/^\S+@\S+\.\S+$/.test(value));
-    }
     // alert(name+" "+value);
     setSignUpobj({ ...signUpobj, [name]: value });
+  }
+
+  function checkValidEmail(event)
+  {
+    setIsValid(/^\S+@\S+\.\S+$/.test(event.target.value));
   }
 
   async function signUp() {
@@ -56,7 +58,7 @@ export default function SignUp() {
                   type="email"
                   autoComplete="email"
                   required disabled={!isValid}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={doUpdate}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" onChange={doUpdate} onBlur={checkValidEmail}
                 />
               </div>
               {!isValid && (
